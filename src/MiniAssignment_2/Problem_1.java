18dd7f13c9416d6e0252ecf375084dcc11e8828c
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-23 15:25:36 +0530

    MiniAssignment_2 Problem_1 started

---
+++ src/MiniAssignment_2/Problem_1.java
@@ -0,0 +1,14 @@
+package MiniAssignment_2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Problem_1 {
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+    }
+}
