c66437bc5735ee091e39784cd1761f70f357d751
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-23 11:46:40 +0530

    relative path written

--- src/MiniAssignment1/problem_1.java
+++ src/MiniAssignment1/problem_1.java
@@ -5,7 +5,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class problem_1 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.phptravels.net/hotels");
+    }
+        //xpath of home-//a[text()='Home']
+        //xpath of Flights=//a[@class=' waves-effect'][@href='https://phptravels.net/flights']
+    //xpath of tours=//a[@title=' waves-effect'][@href='https://phptravels.net/tours']
+    //xpath of company=//a[@class=' waves-effect'][@href='company'][text()='Company ']
+    //xpath of signup button-//a[@class='theme-btn theme-btn-small waves-effect']
+    //xpath of login button-//a[@class='theme-btn theme-btn-small theme-btn-transparent ml-1 waves-effect']
+    //xpath of agents drop down=//button[@id='agents']
     }
 }
