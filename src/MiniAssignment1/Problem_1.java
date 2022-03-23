e68a726d89dd025a7837e211b9359af9c408f2a0
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-23 12:01:52 +0530

    Problem2 file created

--- src/MiniAssignment1/Problem_1.java
+++ src/MiniAssignment1/Problem_1.java
@@ -3,7 +3,7 @@ package MiniAssignment1;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class problem_1 {
+public class Problem_1 {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
---
+++ src/MiniAssignment1/Problem_2.java
@@ -0,0 +1,7 @@
+package MiniAssignment1;
+
+public class Problem_2 {
+    public static void main(String[] args) {
+
+    }
+}
