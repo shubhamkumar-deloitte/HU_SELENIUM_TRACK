59e4e3ef14194d5fce90509e11744b9342162a4f
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-23 15:50:47 +0530

    clicked on pricing option till here

--- src/MiniAssignment_2/Problem_1.java
+++ src/MiniAssignment_2/Problem_1.java
@@ -1,22 +1,38 @@
 package MiniAssignment_2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Problem_1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
 
-        driver.manage().window().maximize();
+
         driver.get("https://phptravels.com/demo");
+        driver.manage().window().maximize();
+        Thread.sleep(3000);
+        String parentWindow= driver.getWindowHandle();
 
         String pageTitle= driver.getTitle();
-        //System.out.println(pageTitle);
+        System.out.println("fist page title"+pageTitle);
         checkTitle(pageTitle);
-        driver.quit();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("/html/body/header/div/nav/a[4]")).click();
+        String secondPageTitle= driver.getTitle();
+        System.out.println("second page title"+secondPageTitle);
+        CheckBothTitle(pageTitle,secondPageTitle);
+        Thread.sleep(2000);
+        driver.switchTo().window(parentWindow);
+        System.out.println("printing the current url "+driver.getCurrentUrl());
+        driver.findElement(By.xpath("//nav[@class='clearfix']/child::a[2]")).click();
+        Thread.sleep(1000);
+
+
+        //driver.quit();
     }
 
     public static void checkTitle(String pageTitle)
@@ -27,6 +43,16 @@ public class Problem_1 {
         }else
         {
             System.out.println("FAIL");
+        }
+    }
+    public static void CheckBothTitle(String title1,String title2)
+    {
+        //System.out.println(title1.equals(title2));
+        if(title1.equals(title2))
+        {
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
         }
     }
 }
