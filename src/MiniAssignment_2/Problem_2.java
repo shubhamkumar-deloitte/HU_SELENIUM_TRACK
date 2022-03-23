72e3d3d201098a390c847350f91a4a5fa131e07e
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-23 15:58:41 +0530

    miniAssignment_2 Problem_2 started

---
+++ src/MiniAssignment_2/Problem_2.java
@@ -0,0 +1,17 @@
+package MiniAssignment_2;
+
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Problem_2 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
+        WebDriver driver;
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("headless");
+        driver=new ChromeDriver();
+
+    }
+}
