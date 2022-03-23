1de07ab15d86c20245c3c344231bfbb0e6d5ac27
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-23 13:18:04 +0530

    Problem2_Solution added

--- src/MiniAssignment1/Problem_2.java
+++ src/MiniAssignment1/Problem_2.java
@@ -3,5 +3,9 @@ package MiniAssignment1;
 public class Problem_2 {
     public static void main(String[] args) {
 
+        //preceding
+        //check in calendar icon-//div[contains(@class,'form-group')]//following::div//input[@id='checkin']
+        //check out calendar icon-//div[contains(@class,'form-group')]//following::div//input[@id='checkout']
+        //adults-  //div[contains(@class,'qtyBtn d-flex align-items-center')]//descendant :: i[contains(@class,'la la-minus') ]
     }
 }
