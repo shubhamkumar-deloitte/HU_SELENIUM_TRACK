5532758ab60c3512eb5373a0db3265729dfa12df
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-24 09:33:16 +0530

    drag and dropped

--- src/MiniAssignment_3/Scenario_2.java
+++ src/MiniAssignment_3/Scenario_2.java
@@ -1,7 +1,11 @@
 package MiniAssignment_3;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Scenario_2 {
     public static void main(String[] args) {
@@ -11,5 +15,14 @@ public class Scenario_2 {
         driver.manage().window().maximize();
 
         driver.get("https://jqueryui.com/droppable/");
+
+        driver.switchTo().frame("demo-frame");
+        Actions builder=new Actions(driver);
+        WebElement source= driver.findElement(By.id("draggable"));
+        WebElement dest=driver.findElement(By.id("droppable"));
+
+        builder.dragAndDrop(source,dest).build().perform();
+
+        System.out.println(source.getText());
     }
 }
