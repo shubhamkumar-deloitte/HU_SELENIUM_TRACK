7f795015073b785878e720988ae4a4d03d67efc1
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-24 09:03:22 +0530

    clicked on frames

--- src/MiniAssignment_3/Scenario_1.java
+++ src/MiniAssignment_3/Scenario_1.java
@@ -1,16 +1,22 @@
 package MiniAssignment_3;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scenario_1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[22]/a")).click();
+        Thread.sleep(1000);
+
+
+        driver.quit();
 
 
     }
