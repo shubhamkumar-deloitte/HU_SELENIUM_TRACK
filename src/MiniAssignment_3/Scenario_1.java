7e6d7fa51c6dd0971e3405a0f147795d5d0edeba
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-24 08:55:04 +0530

    chrome opened

---
+++ src/MiniAssignment_3/Scenario_1.java
@@ -0,0 +1,12 @@
+package MiniAssignment_3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scenario_1 {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+    }
+}
